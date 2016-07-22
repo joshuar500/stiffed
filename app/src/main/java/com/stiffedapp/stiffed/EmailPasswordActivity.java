5508372926aca5c8c7a25d544a90298c7cf75043
Joshuar500 <memonkey@gmail.com>
2016-07-21 21:22:35 -0700

    "Don't need google-services.json or dependencies for now"

--- app/src/main/java/com/stiffedapp/stiffed/EmailPasswordActivity.java
+++ app/src/main/java/com/stiffedapp/stiffed/EmailPasswordActivity.java
@@ -2,17 +2,14 @@ package com.stiffedapp.stiffed;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+
 import com.stiffedapp.stiffed.models.User;
 
 
